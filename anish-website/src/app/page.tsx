d255881dd0422ffb29d55ae315fcb1546632173f
Anish Karthik <anishkarthik21@gmail.com>
2023-10-08 03:57:00 -0500

    Undid change as it broke website :(

--- anish-website/src/app/page.tsx
+++ anish-website/src/app/page.tsx
@@ -78,10 +78,6 @@ const inter700 = Inter({ weight: "700", subsets: ["latin"] });
 const inter800 = Inter({ weight: "800", subsets: ["latin"] });
 const inter900 = Inter({ weight: "900", subsets: ["latin"] });
 
-export const metadata = {
-  title: "Anish Karthik's Website",
-  description: "",
-}
 
 export default function Home() {
   const [sliderRef] = useKeenSlider<HTMLDivElement>(
